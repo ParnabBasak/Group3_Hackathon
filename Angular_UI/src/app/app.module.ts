5ece921e0688f01cd0fccf1e6d4c2eb6894fec4b
Parnab <parnab.basak@gmail.com>
2019-11-02 16:00:46 -0500

    Added Login Pages

--- Angular_UI/src/app/app.module.ts
+++ Angular_UI/src/app/app.module.ts
@@ -1,19 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule }          from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
-import { SigninComponent } from './component/signin/signin.component';
+import { SigninComponent } from './components/login/signin/signin.component';
+import { SignupComponent } from './components/login/signup/signup.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
 
+const appRoutes: Routes = [
+  { path: 'signin', component: SigninComponent },
+  { path: 'signup', component: SignupComponent },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     SigninComponent,
+    SignupComponent,
+    HeaderComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    FormsModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: false } // <-- debugging purposes only
+    ),
     MDBBootstrapModule.forRoot()
   ],
   providers: [],
