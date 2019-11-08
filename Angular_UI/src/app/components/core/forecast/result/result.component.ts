3b6fe737eac3818ea1a1835f974cc6f8fb1ae162
Parnab <parnab.basak@gmail.com>
2019-11-07 21:08:52 -0600

    Search Page changes

--- Angular_UI/src/app/components/core/forecast/result/result.component.ts
+++ Angular_UI/src/app/components/core/forecast/result/result.component.ts
@@ -23,7 +23,9 @@ export class ResultComponent implements OnInit {
 
     };
   this.empData = [ {"userId": "123", "title":"title1", "body":"body1"},
-                   {"userId": "456", "title":"title2", "body":"body2"}];
+                   {"userId": "456", "title":"title2", "body":"body2"},
+                   {"userId": "789", "title":"title3", "body":"body3"},
+                   {"userId": "987", "title":"title4", "body":"body4"}];
   this.temp = true;
   this.displayedColumns= ['UserId2', 'title2', 'body2']
   }
