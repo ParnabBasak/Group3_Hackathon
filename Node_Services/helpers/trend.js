98f9ead7be60172e58f05d2a4060b2e0fa80e530
anandabhijit05 <anandabhijit05@gmail.com>
2019-11-08 15:30:37 -0500

    Update trend.js
    Updated getTrend function to return Promise to use async await

--- Node_Services/helpers/trend.js
+++ Node_Services/helpers/trend.js
@@ -1,17 +1,19 @@
 
 exports.getTrend = function(thisChannel, startDate, endDate, trendCollection){
-    trendCollection.find(
+    var promise = new Promise(function(resolve,reject){
+        trendCollection.find(
         {
             channel: thisChannel,
             saleDate: { $gte: startDate, $lt: endDate }
         }
     ).then(function(data){
-        return(data);
+        resolve(data);
     }).catch(function(err){
-        return ({
+        resolve ({
             status: 500,
             msg: 'Something went wrong in finding neverland'
         })
-    })
-    
+    });
+    });
+    return promise;
 }
