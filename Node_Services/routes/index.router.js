64d92259147c5e0399d9955dbe45f6e5990e5199
anandabhijit05 <anandabhijit05@gmail.com>
2019-11-08 16:32:38 -0500

    Changed route to /trend from /gettrend
    Changed route to /trend from /gettrend to avoid get/post method confusion

--- Node_Services/routes/index.router.js
+++ Node_Services/routes/index.router.js
@@ -14,7 +14,7 @@ router.post('/authenticate', ctrlUser.authenticate);
 router.get('/userProfile', jwtHelper.verifyJwtToken, ctrlUser.userProfile);
 
 //Added by Abhijit on 11/7 for trend.controller
-router.post('/gettrend', ctrlTrend.getTrendController);
+router.post('/trend', ctrlTrend.getTrendController);
 
 const DIR = './uploads';
 
