ad97212524b1bbd9b79c6bd8db849a40f01f4e01
Parnab <parnab.basak@gmail.com>
2019-11-09 16:28:42 -0600

    updated new config

--- node.sh
+++ node.sh
@@ -6,7 +6,10 @@ echo "-------------------------"
 cd ./Node_Services
 npm install
 
+echo "-------------------------"
 echo "Process completed with exit code 0"
+echo "-------------------------"
+
 res="$($1)"
 echo "$?"
 echo "$res"
