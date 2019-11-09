2522bbd2d82ed2b3eeae09fc07b903d81da55221
Parnab <parnab.basak@gmail.com>
2019-11-09 16:21:28 -0600

    updated new config

--- angular.sh
+++ angular.sh
@@ -1,6 +1,21 @@
 #!/bin/sh
 
-echo "hello angular"
+echo "-------------------------"
+echo "This is Anngular Building"
+echo "-------------------------"
+cd ./Angular_UI
+npm install
+echo "-------------------------"
+echo "Running npm run test"
+echo "-------------------------"
+npm run test -- --watch=false --no-progress --browsers=ChromeHeadlessNoSandbox
+
+echo "-------------------------"
+echo "Running ng build --prod"
+echo "-------------------------"
+ng build --prod
+
+echo "Process completed with exit code 0"
 res="$($1)"
 echo "$?"
 echo "$res"
