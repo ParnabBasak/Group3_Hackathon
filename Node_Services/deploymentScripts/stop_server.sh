b0f829c2ada3e5c5fedc366c8a18febeb78f8472
Parnab <parnab.basak@gmail.com>
2019-11-10 11:29:14 -0600

    Added config for AWS deploy

--- Node_Services/deploymentScripts/stop_server.sh
+++ Node_Services/deploymentScripts/stop_server.sh
@@ -5,4 +5,4 @@ echo "Executing ApplicationStop hook. In stop_server.sh "
 echo "----------------------------------------------------"
 
 cd /home/ec2-user/Node_Services
-pm2 stop node-server
+#pm2 stop node-server
