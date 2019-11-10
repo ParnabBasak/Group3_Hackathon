eff23c93575c1dea183b8156a1a258c443b7ec99
Parnab <parnab.basak@gmail.com>
2019-11-10 13:42:37 -0600

    Added config for AWS deploy

--- Node_Services/deploymentScripts/unzipfiles.sh
+++ Node_Services/deploymentScripts/unzipfiles.sh
@@ -5,6 +5,7 @@ echo "------------------------------------------"
 echo "Executing BeforeInstall hook. In unzip.sh "
 echo "------------------------------------------"
 cd /home/ec2-user/Node_Services
+echo whoami
 echo "-------------------------"
 echo "Run npm install"
 echo "-------------------------"
