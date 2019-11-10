e02179aade013ded222f47b4bf7cc1ee28055211
Parnab <parnab.basak@gmail.com>
2019-11-10 09:47:06 -0600

    Added config for AWS deploy

--- Node_Services/deploymentScripts/start_server.sh
+++ Node_Services/deploymentScripts/start_server.sh
@@ -3,3 +3,5 @@
 echo "----------------------------------------------------"
 echo "Executing ApplicationStart hook. In start_server.sh "
 echo "----------------------------------------------------"
+cd /home/ec2-user/Node_Services
+pm2 start ./bin/www --name node-server
--- Node_Services/deploymentScripts/stop_server.sh
+++ Node_Services/deploymentScripts/stop_server.sh
@@ -3,3 +3,6 @@
 echo "----------------------------------------------------"
 echo "Executing ApplicationStop hook. In stop_server.sh "
 echo "----------------------------------------------------"
+
+cd /home/ec2-user/Node_Services
+pm2 stop node-server
