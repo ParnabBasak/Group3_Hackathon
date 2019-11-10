e71616b2926dd2eb4d80721f9b3b995e56401ae9
Parnab <parnab.basak@gmail.com>
2019-11-10 14:55:12 -0600

    Changed config for prod deployment

--- Node_Services/deploymentScripts/start_server.sh
+++ Node_Services/deploymentScripts/start_server.sh
@@ -4,6 +4,7 @@ source /home/ec2-user/.bash_profile
 echo "----------------------------------------------------"
 echo "Executing ApplicationStart hook. In start_server.sh "
 echo "----------------------------------------------------"
+cp .env ./Node_Services
 cd /home/ec2-user/Node_Services
 pm2 start ./bin/www --name node-server
 echo "----------------------------------------------------"
