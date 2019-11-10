bf0465d944c47daae8da5c7383ae89c500c6621a
Parnab <parnab.basak@gmail.com>
2019-11-10 13:05:58 -0600

    Added config for AWS deploy

--- Node_Services/deploymentScripts/unzipfiles.sh
+++ Node_Services/deploymentScripts/unzipfiles.sh
@@ -1,26 +1,13 @@
 #!/bin/sh
 
-source /home/ec2-user/.bash_profile
-echo "line1"
-sudo npm --version
 echo "------------------------------------------"
 echo "Executing BeforeInstall hook. In unzip.sh "
 echo "------------------------------------------"
-echo "-------------------------"
-echo "Present working directory before cd"
-echo "-------------------------"
-echo $PWD
-echo "-------------------------"
 cd /home/ec2-user/Node_Services
 echo "-------------------------"
-echo "Present working directory after cd"
+echo "Run npm install"
 echo "-------------------------"
-echo $PWD
-echo "-------------------------"
-echo "Run npm version"
-echo "-------------------------"
-npm --version
-#npm install
+npm install
 echo "-------------------------"
 echo "Completed npm install"
 echo "-------------------------"
